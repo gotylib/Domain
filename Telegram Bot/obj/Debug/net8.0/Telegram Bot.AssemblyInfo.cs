2445c3f1c5ce9d682475193c94b98043ea62aced
Gleb <mixalev702@mail.ru>
2024-10-11 13:40:13 +0300

    Настроен .gitignore

--- Telegram Bot/obj/Debug/net8.0/Telegram Bot.AssemblyInfo.cs
+++ Telegram Bot/obj/Debug/net8.0/Telegram Bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telegram Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c50b17a39879cd4bee52e89848543c04e695af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+303919bd02bc28fa3e7e51f20b7ce05b4e37a6da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telegram Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telegram Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
